295d3157ce32fa6acfc99a780f53a538e013c855
Gustavo Luis Ferreira Silva <gustavo.l.silva37@aluno.senai.br>
2026-08-11 14:31:55 -0300

    fix: usar entrada Express oficial na Vercel

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-import app from './backend/src/server.js';
+import express from 'express';
+import backendApp from './backend/src/server.js';
+
+const app = express();
+app.use(backendApp);
 
 export default app;
